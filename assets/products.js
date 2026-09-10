b418f6af2af6c38f5c84fe91e9f98ab73333a65b
11ONGOAL <vasiliualexandru555@yahoo.com>
2026-09-10 11:17:54 +0300

    Update products.js

--- assets/products.js
+++ assets/products.js
@@ -707,4 +707,16 @@ window.PRODUCTS = [
     "image": "assets/images/71WJSaYK1yL._AC_SL1200_.jpg",
     "featured": false
   },
+  {
+    "id": 60,
+    "slug": "set-filetare-tevi-pvc-2-in-1-interior-exterior",
+    "name": "Set Filetare Țevi PVC 2-în-1, Interior și Exterior (set 3 buc.)",
+    "description": "Set practic pentru realizarea <strong>filetelor interioare și exterioare pe țevi",
+    "price": "16 Lei",
+    "stock": "493",
+    "video": "",
+    "category": "Casă și grădină",
+    "image": "assets/images/71WJSaYK1yL._AC_SL1200_.jpg",
+    "featured": false
+  },
 ];
