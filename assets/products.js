f13bfbe5b1dcddf5f471a1e4a03a56305cb395a6
11ONGOAL <vasiliualexandru555@yahoo.com>
2026-09-23 11:33:09 +0300

    Update products.js

--- assets/products.js
+++ assets/products.js
@@ -1,4 +1,16 @@
 window.PRODUCTS = [
+       {
+    "id": 64,
+    "slug": "ultra-adeziv-anti-scurgeri-strongfix",
+    "name": "Ultra Adeziv Anti-Scurgeri StrongFix ",
+    "description": "Transformă repararea scurgerilor într-o sarcină simplă și eficientă cu agentul nostru rezistent la apă și scurgeri.",
+    "price": "0 Lei",
+    "stock": "0 Buc.",
+    "video": "",
+    "category": "Casă și Grădină",
+    "image": "assets/images/Media1_dc57a0f9-af5b-4c76-913b-06cd214261d1.webp",
+    "featured": false
+  },
    {
     "id": 59,
     "slug": "patura-impermeabila",
@@ -741,18 +753,6 @@ window.PRODUCTS = [
     "video": "",
     "category": "Beauty",
     "image": "assets/images/crema-pentru-hemoroizi-20g.webp",
-    "featured": false
-  },
-    {
-    "id": 64,
-    "slug": "ultra-adeziv-anti-scurgeri-strongfix",
-    "name": "Ultra Adeziv Anti-Scurgeri StrongFix ",
-    "description": "Transformă repararea scurgerilor într-o sarcină simplă și eficientă cu agentul nostru rezistent la apă și scurgeri.",
-    "price": "0 Lei",
-    "stock": "0 Buc.",
-    "video": "",
-    "category": "Casă și Grădină",
-    "image": "assets/images/Media1_dc57a0f9-af5b-4c76-913b-06cd214261d1.webp",
     "featured": false
   },
 ];
